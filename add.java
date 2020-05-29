b531e7446ac6f0018a67ab3b76549c5fd23ea2ca
Shivangi <sviradiya@njcu.edu>
2020-05-29 22:08:34 +0000

    This is my java program

---
+++ add.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class add {
+        
+       public static void main(String[] args)
+       {
+            
+           int num1 = 10, num2 = 10, sum;
+           sum = num1 + num2;
+
+           System.out.println("Sum of these numbers:" +sum);
+
+       }
+}       
